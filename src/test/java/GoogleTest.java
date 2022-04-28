2bdf6a59317b2bf5fbdc8027906f35530e5222c8
Aleksandr Butakov <anbngm@gmail.com>
2022-04-28 19:41:48 +0500

    Connect with Allure TestOps

---
+++ src/test/java/GoogleTest.java
@@ -0,0 +1,43 @@
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GoogleTest {
+
+    @Test
+    @AllureId("9742")
+    @DisplayName("Autorization via Google")
+    @Tags({@Tag("critical"), @Tag("web")})
+    @Story("External Auth")
+    @Owner("allure8")
+    @Feature("Auth")
+    public void testAuth() {
+        step("Open the main page", () -> {
+            Selenide.open("https://google.com");
+        });
+        step("Choosing the autorization via Google");
+        step("Login in as user Ivan", () -> {
+            step("Enter the login ivan@google.com");
+            step("Enter the password 1234");
+            step("Click the log in button");
+        });
+        step("We check that you have logged in correctly", () -> {
+            step("We are on the main page of the site");
+            step("The user is logged in", () -> {
+                assertTrue(false);
+            });
+            step("Login Ivan");
+        });
+        step("Log out");
+    }
+
+}
